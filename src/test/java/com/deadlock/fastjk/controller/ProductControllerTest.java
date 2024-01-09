7d1b5090966ecb8887686f3b3628d2141dc5e0a0
maycon <mayconfelipe164@gmail.com>
2024-01-09 20:19:58 -0300

    add sonar configuration

--- src/test/java/com/deadlock/fastjk/controller/ProductControllerTest.java
+++ src/test/java/com/deadlock/fastjk/controller/ProductControllerTest.java
@@ -1,13 +1,35 @@
 package com.deadlock.fastjk.controller;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Test;
+import com.deadlock.fastjk.core.service.ProductService;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest
+@AutoConfigureMockMvc
 class ProductControllerTest {
 
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new ProductController(productService)).build();
+    }
+
     @Test
-    void givenInvalidBodyRequest_thenReturns400() {
+    void givenInvalidBodyRequest_thenReturns400() throws Exception {
         String bodyRequest = """
                 {
                     "name": "product A",
@@ -16,12 +38,10 @@ class ProductControllerTest {
                 }
                 """;
 
-        RestAssured.given()
-                .contentType(ContentType.JSON)
-                .body(bodyRequest)
-                .when()
-                .post("/products")
-                .then()
-                .statusCode(400);
+        mockMvc.perform(MockMvcRequestBuilders.post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bodyRequest))
+                .andExpect(status().isBadRequest());
+
     }
 }
